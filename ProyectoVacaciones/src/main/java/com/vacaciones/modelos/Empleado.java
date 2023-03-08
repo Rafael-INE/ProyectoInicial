18422e66cf134685e51cd880e276cc8a846cdf7c
mario.aparicio.ext <mario.aparicio.ext@IBLAESMADC00394.IBL.INETUM>
2023-03-08 13:20:26 +0100

    Modifico la clase de Empleado

--- ProyectoVacaciones/src/main/java/com/vacaciones/modelos/Empleado.java
+++ ProyectoVacaciones/src/main/java/com/vacaciones/modelos/Empleado.java
@@ -1,5 +1,49 @@
 package com.vacaciones.modelos;
 
 public class Empleado {
-
+	String email;
+	String pwd;
+	String first_name;
+	String last_name;
+	int roles;
+	
+	public Empleado(String email, String pwd, String first_name, String last_name, int roles) {
+		this.email = email;
+		this.pwd = pwd;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.roles = roles;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public int getRoles() {
+		return roles;
+	}
+	public void setRoles(int roles) {
+		this.roles = roles;
+	}
+	
+	
 }
