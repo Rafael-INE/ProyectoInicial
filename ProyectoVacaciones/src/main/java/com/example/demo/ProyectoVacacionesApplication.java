ccd49869b2fa14822901f98d327b58a2a3b24d1c
mario.aparicio.ext <mario.aparicio.ext@IBLAESMADC00394.IBL.INETUM>
2023-03-08 12:26:21 +0100

    prueba mensaje

--- ProyectoVacaciones/src/main/java/com/example/demo/ProyectoVacacionesApplication.java
+++ ProyectoVacaciones/src/main/java/com/example/demo/ProyectoVacacionesApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoVacacionesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hola");
 		SpringApplication.run(ProyectoVacacionesApplication.class, args);
 	}
+	
 
 }
