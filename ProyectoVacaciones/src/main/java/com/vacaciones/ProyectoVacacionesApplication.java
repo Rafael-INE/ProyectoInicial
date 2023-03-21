6c9dc671948477e929ff5b4cecdd8caac065c953
Rafael-INE <rafael.alonso.ext@inetum.com>
2023-03-21 13:35:25 +0100

    Main app runner

--- ProyectoVacaciones/src/main/java/com/vacaciones/ProyectoVacacionesApplication.java
+++ ProyectoVacaciones/src/main/java/com/vacaciones/ProyectoVacacionesApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoVacacionesApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
-		System.out.println("Rafael");
 		SpringApplication.run(ProyectoVacacionesApplication.class, args);
 	}
 	
